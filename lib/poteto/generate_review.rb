3cb65887c2b6b6ea8f124db37393955abf823da5
OPhamster <neel.maitra@clarisights.com>
2024-11-19 05:32:36 +0000

    fix(gen-review): Reuse overcommits strat to decide relevancy
    Made the behavior similar to overcommits strategy since it's been proven before[1]
    
    [1]: https://github.com/sds/overcommit/blob/eff94a7f058d9eeca3f30e069fadba5a82f43da2/lib/overcommit/git_repo.rb#L68C1-L87C4

--- lib/poteto/generate_review.rb
+++ lib/poteto/generate_review.rb
@@ -12,20 +12,9 @@ module Poteto
       @exclude = exclude
       @reviews = []
     end
-  end
-
-  class GenerateRubocopReview < GenerateReview
-    def initialize(commit_id, exclude: ["spec/", "test/"])
-      super(commit_id, exclude: exclude)
-      call
-    end
-
-    def call
-      @reviews << rubocop_on_changes(files_change_ranges_for_rubocop)
-    end
 
     def changed_files
-      git_args = ["git", "diff", "--diff-filter=ACM", "--name-only", "#{commit_id}..HEAD"]
+      git_args = ['git', 'diff', '--diff-filter=ACM', '--name-only', "#{commit_id}..HEAD"]
       command(git_args)
     end
 
@@ -52,21 +41,33 @@ module Poteto
     def change_ranges(files)
       files.to_h do |f|
         meta = command(["git diff #{commit_id}..HEAD -U0 -- #{f} | grep '^@@'"])
-        line_nos = meta.flat_map do |m|
-          remove, add = m.match(/^@@(.*)@@/)[1].strip.split()
-          line_nos = []
+        line_nos = meta.map do |m|
+          remove, add = m.match(/^@@(.*)@@/)[1].strip.split
+          hunk_line_nos = []
           unless remove.nil?
-            remove_start_line, _ = remove.split(",")
-            line_nos << remove_start_line.to_i.abs
+            remove_start_line, = remove.split(',')
+            hunk_line_nos << remove_start_line.to_i.abs
           end
           unless add.nil?
-            add_end_line, _ = add.split(",")
-            line_nos << add_end_line.to_i.abs
+            # the separation into components is just for readability
+            add_end_line, additions = add.split(',')
+            hunk_line_nos << [add_end_line, additions].map(&:to_i).sum
           end
-          line_nos
+          hunk_line_nos.minmax
         end
-        [f, [line_nos.min, line_nos.max]]
+        [f, line_nos]
       end
+    end
+  end
+
+  class GenerateRubocopReview < GenerateReview
+    def initialize(commit_id, exclude: ['spec/', 'test/'])
+      super(commit_id, exclude: exclude)
+      call
+    end
+
+    def call
+      @reviews << rubocop_on_changes(files_change_ranges_for_rubocop)
     end
 
     def files_change_ranges_for_rubocop
@@ -74,14 +75,17 @@ module Poteto
     end
 
     def rubocop_on_changes(file_change_ranges)
-      rubocop_args = ["bundle", "exec", "rubocop", "--force-exclusion", "-fg", "-E", "--display-only-fail-level-offenses"]
-      file_change_ranges.flat_map do |f, f_change_range|
+      rubocop_args = ['bundle', 'exec', 'rubocop', '--force-exclusion', '-fg', '-E',
+                      '--display-only-fail-level-offenses']
+      file_change_ranges.flat_map do |f, f_change_ranges|
         rc_args = rubocop_args + [f]
         rubocop_failures = command(rc_args, expected_status_code: 256)
         rc_relevant_failures = rubocop_failures.filter do |f|
-          if f.start_with?("::error")
-            failure_line = f.split(",")[1].match(/line=(\d+)/)[1].to_i
-            f_change_range[0] <= failure_line && f_change_range[1] >= failure_line
+          if f.start_with?('::error')
+            failure_line = f.split(',')[1].match(/line=(\d+)/)[1].to_i
+            f_change_ranges.any? do |(hunk_start, hunk_end)|
+              hunk_start <= failure_line && hunk_end >= failure_line
+            end
           else
             false
           end
