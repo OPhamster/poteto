f8f235b0956c455ebb113d35c3121cf049bfd471
OPhamster <neel.maitra@clarisights.com>
2024-11-30 14:54:25 +0000

    rft(review): Give Reviews the ability to capture commits
    * Github validates the pr-comments request against the:
      * commit_id
      * files changed by the commit_id
      * hunks changed by the commit_id (maybe)
    * So for every change we need to be able to get the corresponding commit it was
      changed in, we're doing some low effort work to get the last commit that changed
      a file. If this works we'll start with this OTHERWISE it's a more involved
      process of getting the commit id for each hunk we're selecting.

--- lib/poteto/post_review.rb
+++ lib/poteto/post_review.rb
@@ -4,29 +4,30 @@ require 'octokit'
 
 module Poteto
   class PostReview
-    attr_reader :commit_id, :repo, :config
+    attr_reader :commit_id, :repo, :reviewer
     attr_accessor :post_lambda, :rate_limit_lambda
 
     # def initialize(repo, commit_id, pr_id, post_lambda, rate_limit_lambda: nil)
-    def initialize(config)
-      @commit_id = config.commit_id
-      @pr_id = config.pr_id
-      @repo = config.repo
-      @config = config
+    def initialize(reviewer)
+      @commit_id = reviewer.commit_id
+      @pr_id = reviewer.pr_id
+      @repo = reviewer.repo
+      @reviewer = reviewer
     end
 
     def call(reviews)
       reviews.each do |review|
-        post_lambda.call(config, review)
+        post_lambda.call(reviewer, review)
       end
     end
 
     def post_lambda
       return @post_lambda unless @post_lambda.nil?
 
-      @post_client = ::Octokit::Client.new(access_token: config.access_token)
-      @post_lambda = proc do |config, review|
-        @post_client.create_pull_request_comment(config.repo, config.pr_id, review.comment, config.commit_id,
+      @post_client = ::Octokit::Client.new(access_token: reviewer.access_token)
+      @post_lambda = proc do |reviewer, review|
+        commit_id = review.commit_id || reviewer.commit_id
+        @post_client.create_pull_request_comment(reviewer.repo, reviewer.pr_id, review.comment, commit_id,
                                                  review.path, review.line)
       end
     end
--- lib/poteto/review.rb
+++ lib/poteto/review.rb
@@ -2,19 +2,20 @@
 
 module Poteto
   class Review
-    attr_accessor :path, :line, :comment
+    attr_accessor :path, :line, :comment, :commit_id
     attr_reader :raw_data
 
-    def initialize(raw_data, file_name: nil)
+    def initialize(raw_data, file_name: nil, commit_id: nil)
       @raw_data = raw_data
       @file_name = file_name
       @path = file_name unless file_name.nil?
+      @commit_id = commit_id unless commit_id.nil?
     end
   end
 
   class RubocopReview < Review
-    def initialize(raw_data, file_name: nil)
-      super(raw_data, file_name: file_name)
+    def initialize(raw_data, file_name: nil, commit_id: nil)
+      super(raw_data, file_name: file_name, commit_id: commit_id)
       parse
     end
 
@@ -22,7 +23,7 @@ module Poteto
       path, @line, @comment  = @raw_data.split(",")
       @path ||= path.match(/file=(.*)$/)[1]
       @line = @line.match(/line=(\d+)/)[1].to_i
-      @comment = @comment.split("::").last
+      @comment = @comment.split("::")[2..]
       nil
     end
   end
